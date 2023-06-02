6d3e98379e0ba70f180c1f0169b18a806df3889a
Frederik Norlyk <frederiknorlyk@gmail.com>
2023-06-02 14:46:17 +0200

    Added sqlite db when running locally

--- core/settings.py
+++ core/settings.py
@@ -84,11 +84,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600    
-    )
+
 }
+    
+if ('RENDER' in os.environ) :
+    DATABASES['default'] = dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+else :
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
 
 
 # Password validation
