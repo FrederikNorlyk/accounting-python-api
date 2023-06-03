4ea9f301642d8de4c4fb657c55f9a228076ca715
Frederik Norlyk <frederiknorlyk@gmail.com>
2023-06-03 20:04:52 +0200

    The user endpoint can now add users

--- accounting/serializers/user_serializer.py
+++ accounting/serializers/user_serializer.py
@@ -1,16 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-from accounting.models.expense import Expense
-from accounting.models.merchant import Merchant
-from accounting.models.project import Project
-
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    expenses = serializers.PrimaryKeyRelatedField(many=True, queryset=Expense.objects.all())
-    projects = serializers.PrimaryKeyRelatedField(many=True, queryset=Project.objects.all())
-    merchants = serializers.PrimaryKeyRelatedField(many=True, queryset=Merchant.objects.all())
-
     class Meta:
         model = User
-        fields = ['id', 'username', 'expenses', 'projects', 'merchants', 'url']
+        fields = ['id', 'username', 'password', 'url']
--- accounting/views/user_view_set.py
+++ accounting/views/user_view_set.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 
 from accounting.serializers.user_serializer import UserSerializer
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        validated_data = serializer.validated_data
+        validated_data['password'] = make_password(validated_data['password'])
+        super().perform_create(serializer)
