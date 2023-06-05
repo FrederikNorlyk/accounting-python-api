8d8febdcee67bb1ff2f028deb5021056d75cd8a8
Frederik Norlyk <frederiknorlyk@gmail.com>
2023-06-05 20:53:57 +0200

    Added total_amount and number_of_expenses to merchant

--- accounting/serializers/merchant_serializer.py
+++ accounting/serializers/merchant_serializer.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
-from accounting.models.expense import Expense
+from django.db.models import Sum, Count
+
 from accounting.models.merchant import Merchant
 
 
 class MerchantSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.ReadOnlyField(source="user.id")
+    total_amount = serializers.SerializerMethodField()
+    number_of_expenses = serializers.SerializerMethodField()
 
     class Meta:
         model = Merchant
-        fields = ['id', 'name', 'user', 'url']
+        fields = ['id', 'name', 'user', 'total_amount', 'number_of_expenses', 'url']
+
+    def get_total_amount(self, obj):
+        return obj.expenses.aggregate(Sum('amount'))['amount__sum']
+    
+    def get_number_of_expenses(self, obj):
+        return obj.expenses.aggregate(Count('id'))['id__count']
